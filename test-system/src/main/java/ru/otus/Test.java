b66ccc408e41c31c2e32e0b797da558c6fa1226a
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-11-08 21:47:33 +0200

    TASK-01: Used junit for assertions in tests

--- test-system/src/main/java/ru/otus/Test.java
+++ test-system/src/main/java/ru/otus/Test.java
@@ -1,25 +1,34 @@
 package ru.otus;
 
+import org.opentest4j.AssertionFailedError;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Test<I, O> {
 
-    private final String name;
+	private final String name;
 
-    private final TestDataSource dataSource;
+	private final TestDataSource dataSource;
 
-    public Test(String name, TestDataSource dataSource) {
-        this.name = name;
-        this.dataSource = dataSource;
-    }
+	public Test(String name, TestDataSource dataSource) {
+		this.name = name;
+		this.dataSource = dataSource;
+	}
 
-    public void run(TestCase<I, O> testCase) {
-        System.out.format("\nRun test \"%s\"\n", name);
+	public void run(TestCase<I, O> testCase) {
+		System.out.println("\nTest \"" + name + "\"");
 
-        dataSource.forEachRemaining(testData -> {
-            final long startTime = System.currentTimeMillis();
-            final O result = testCase.execute(testCase.getInput(testData.getInput()));
-            final long executionTime = System.currentTimeMillis() - startTime;
-            final boolean isSuccess = result.equals(testCase.getOutput(testData.getOutput()));
-            System.out.format("%s, Execution time: %d ms\n", isSuccess ? "SUCCESS" : "FAILED", executionTime);
-        });
-    }
+		dataSource.forEachRemaining(testData -> {
+			final long startTime = System.currentTimeMillis();
+			final O result = testCase.execute(testCase.getInput(testData.getInput()));
+			final long executionTime = System.currentTimeMillis() - startTime;
+
+			try {
+				assertEquals(testCase.getOutput(testData.getOutput()), result);
+				System.out.println("SUCCESS, Execution time " + executionTime + " ms");
+			} catch (AssertionFailedError e) {
+				System.out.println("FAILED, " + e.getMessage());
+			}
+		});
+	}
 }
