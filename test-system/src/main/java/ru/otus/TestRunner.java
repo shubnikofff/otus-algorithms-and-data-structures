0ceca2f5ee0824b755c27b4edb4c684bc99103f0
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-11-14 13:38:54 +0200

    TASK-02: Implemented interface Iterator in TestDataSource

--- test-system/src/main/java/ru/otus/TestRunner.java
+++ test-system/src/main/java/ru/otus/TestRunner.java
@@ -9,22 +9,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestRunner {
 
-	public static <I, O> void run(Testable<I, O> testable, TestDataSource  dataSource) {
-		System.out.println("\nTest \"" + testable.getName() + "\"");
+    public static <I, O> void run(Testable<I, O> testable, TestDataSource dataSource) {
+        System.out.println("\nTest \"" + testable.getName() + "\"");
 
-		dataSource.forEachRemaining(testData -> {
-			final I arguments = testable.getArguments(testData.getInput());
-			final Instant startTime = Instant.now();
-			final O result = testable.execute(arguments);
-			final long executionTime = Duration.between(startTime, Instant.now()).toMillis();
+        for (TestData testData : dataSource) {
+            final I arguments = testable.getArguments(testData.getInput());
+            final Instant startTime = Instant.now();
+            final O result = testable.execute(arguments);
+            final long executionTime = Duration.between(startTime, Instant.now()).toMillis();
 
-			try {
-				final O expectedResult = testable.getExpectedResult(testData.getOutput());
-				assertEquals(expectedResult, result);
-				System.out.println("SUCCESS, Execution time " + executionTime + " ms");
-			} catch (AssertionFailedError e) {
-				System.out.println("FAILED, " + e.getMessage() + ",  Execution time: " + executionTime + " ms");
-			}
-		});
-	}
+            try {
+                final O expectedResult = testable.getExpectedResult(testData.getOutput());
+                assertEquals(expectedResult, result);
+                System.out.println("SUCCESS, Execution time " + executionTime + " ms");
+            } catch (AssertionFailedError e) {
+                System.out.println("FAILED, " + e.getMessage() + ",  Execution time: " + executionTime + " ms");
+            }
+        }
+    }
 }
