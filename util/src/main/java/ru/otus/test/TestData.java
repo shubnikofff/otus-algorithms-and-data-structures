532244f76a68604cc10e2aae2bd1ac8ba4293a72
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-11-20 14:27:15 +0200

    Task-03 Created module "util"

---
+++ util/src/main/java/ru/otus/test/TestData.java
@@ -0,0 +1,23 @@
+package ru.otus.test;
+
+import java.util.List;
+
+public class TestData {
+
+    private final List<String> input;
+
+    private final List<String> output;
+
+    public TestData(List<String> input, List<String> output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public List<String> getInput() {
+        return input;
+    }
+
+    public List<String> getOutput() {
+        return output;
+    }
+}
