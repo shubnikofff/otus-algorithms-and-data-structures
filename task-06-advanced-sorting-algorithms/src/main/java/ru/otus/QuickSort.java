5e695dc4a234d776504641b2833d74310b18edda
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-12-16 19:46:35 +0200

    Task-06: Implemented Quick sort

---
+++ task-06-advanced-sorting-algorithms/src/main/java/ru/otus/QuickSort.java
@@ -0,0 +1,37 @@
+package ru.otus;
+
+public class QuickSort {
+
+    public static void sort(int[] array) {
+        sort(0, array.length - 1, array);
+    }
+
+    private static void sort(int left, int right, int[] array) {
+        if (left >= right) {
+            return;
+        }
+
+        final int center = partition(left, right, array);
+        sort(left, center - 1, array);
+        sort(center + 1, right, array);
+    }
+
+    private static int partition(int left, int right, int[] array) {
+        final int pivot = array[right];
+        int a = left - 1;
+
+        for (int m = left; m <= right; m++) {
+            if (array[m] <= pivot) {
+                swap(++a, m, array);
+            }
+        }
+
+        return a;
+    }
+
+    private static void swap(int from, int to, int[] array) {
+        final int buffer = array[from];
+        array[from] = array[to];
+        array[to] = buffer;
+    }
+}
