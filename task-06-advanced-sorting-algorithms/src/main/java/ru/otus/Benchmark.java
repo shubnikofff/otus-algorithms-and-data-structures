2f2d72b0e8a4d7c1e50dae213e2a3612b91e76de
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-01-04 12:33:47 +0200

    Task-06: Created benchmark for Counting sort

--- task-06-advanced-sorting-algorithms/src/main/java/ru/otus/Benchmark.java
+++ task-06-advanced-sorting-algorithms/src/main/java/ru/otus/Benchmark.java
@@ -1,6 +1,7 @@
 package ru.otus;
 
 import ru.otus.file.FileGenerator;
+import ru.otus.sort.CountingSort;
 import ru.otus.sort.MergeSort;
 
 import java.io.File;
@@ -12,23 +13,42 @@ import java.util.UUID;
 
 public class Benchmark {
 
+	private static final int MAX_VALUE = (int) Math.pow(2, 16) - 1;
+
+	public static final int[] NUMBER_SIZE = {(int) 1e6, (int) 1e7, (int) 1e8, (int) 1e9};
+
 	public static void main(String[] args) throws IOException {
-		run((int)1e6);
-		run((int)1e7);
-		run((int)1e8);
-		run((int)1e9);
+		for (int size : NUMBER_SIZE) {
+			runMergeSort(size);
+			runCountingSort(size);
+		}
 	}
 
-	private static void run(int numbersSize) throws IOException {
+	private static void runMergeSort(int numbersSize) throws IOException {
 		System.out.println("Run Merge Sort on file with " + numbersSize + " numbers");
 
 		final File file = new File(UUID.randomUUID().toString());
-		FileGenerator.generate(numbersSize, file);
+		FileGenerator.generate(numbersSize, MAX_VALUE, file);
 
 		final Instant startTime = Instant.now();
 		MergeSort.sort(file);
 		System.out.println("Duration " + Duration.between(startTime, Instant.now()).getSeconds() + " seconds");
 
 		Files.deleteIfExists(file.toPath());
+	}
+
+	private static void runCountingSort(int numbersSize) throws IOException {
+		System.out.println("Run Counting Sort on file with " + numbersSize + " numbers");
+
+		final File inputFile = new File(UUID.randomUUID().toString());
+		final File outputFile = new File(UUID.randomUUID().toString());
+		FileGenerator.generate(numbersSize, MAX_VALUE, inputFile);
+
+		final Instant startTime = Instant.now();
+		CountingSort.sort(inputFile, outputFile);
+		System.out.println("Duration " + Duration.between(startTime, Instant.now()).getSeconds() + " seconds");
+
+		Files.deleteIfExists(inputFile.toPath());
+		Files.deleteIfExists(outputFile.toPath());
 	}
 }
