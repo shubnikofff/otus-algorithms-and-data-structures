2f5d74ac2791e2043a2aae8eac47c32331f50fee
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-01-05 21:02:32 +0200

    Task-07: Implemented method insert of binary search tree

---
+++ task-07-binary-search-tree/src/main/java/ru/otus/BinarySearchTree.java
@@ -0,0 +1,43 @@
+package ru.otus;
+
+import static java.lang.String.format;
+
+public class BinarySearchTree {
+
+	private Node root;
+
+	public void insert(int value) {
+		root = insert(root, value);
+	}
+
+	private Node insert(Node node, int value) {
+		if (node == null) {
+			return new Node(value);
+		}
+
+		if (value == node.getValue()) {
+			throw new IllegalArgumentException(format("Node with value %d already exists", value));
+		}
+
+		if (value < node.getValue()) {
+			node.setLeft(insert(node.getLeft(), value));
+		} else {
+			node.setRight(insert(node.getRight(), value));
+		}
+
+		return node;
+	}
+
+	private void insertRight(Node parent, Node child) {
+
+	}
+
+	public void remove(int value) {
+
+	}
+
+	public boolean search(int value) {
+		return true;
+	}
+
+}
