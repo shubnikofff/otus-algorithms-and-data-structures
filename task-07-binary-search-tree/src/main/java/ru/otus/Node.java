f27aa8fa7b70c945daa60741d62e171467d6eeab
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-01-05 21:01:40 +0200

    Task-07: Created class Node

---
+++ task-07-binary-search-tree/src/main/java/ru/otus/Node.java
@@ -0,0 +1,35 @@
+package ru.otus;
+
+class Node {
+
+	private Node left;
+
+	private Node right;
+
+	private final int value;
+
+	public Node(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+}
---
+++ task-07-binary-search-tree/src/test/java/ru/otus/BinarySearchTreeTest.java
@@ -0,0 +1,21 @@
+package ru.otus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTreeTest {
+
+	@Test
+	void insert() {
+
+	}
+
+	@Test
+	void remove() {
+	}
+
+	@Test
+	void search() {
+	}
+}
