173ca0d6c5e64e0306841a7f679dccef74f75f29
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-02-07 20:46:34 +0200

    Task-08 Implemented methods get and put of HashTable

--- task-08-hash-tables/src/main/java/ru/otus/HashTable.java
+++ task-08-hash-tables/src/main/java/ru/otus/HashTable.java
@@ -2,64 +2,110 @@ package ru.otus;
 
 public class HashTable<K, V> {
 
-	private static final int INITIAL_CAPACITY = 10;
+    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
 
-	private static final float LOAD_FACTOR = 0.5f;
+    private static final float LOAD_FACTOR = 0.5f;
 
-	private int size = 0;
+    private static final int MAXIMUM_CAPACITY = 1 << 30;
 
-	private Entry<K, V>[] entries = new Entry[INITIAL_CAPACITY];
+    private int size;
 
-	public int size() {
-		return size;
-	}
+    private Entry<K, V>[] entries;
 
-	public V put(K key, V value) {
-		final int entryIndex = getNearestEmptyIndex(key);
-		entries[entryIndex] = new Entry<>(key, value);
+    public HashTable(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("Illegal initial capacity: " + initialCapacity);
+        }
 
-		size++;
-		// TODO rehash
+        entries = new Entry[initialCapacity];
+        size = 0;
+    }
 
-		return value;
-	}
+    public HashTable() {
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
 
-	public V get(K key) {
-		final int keyHash = key.hashCode();
+    public int size() {
+        return size;
+    }
 
-		return entries[keyHash].value;
-	}
+    public V put(K key, V value) {
+        final int entriesLength = entries.length;
+        final int startIndex = getIndex(key);
 
-	private int hash(K key) {
-		return key.hashCode() % entries.length;
-	}
+        for (int index = startIndex; index != startIndex - 1; index++) {
+            if (entries[index] == null) {
+                entries[index] = new Entry<>(key, value);
+                if (++size > entries.length * LOAD_FACTOR) {
+                    resize();
+                }
+                return null;
+            }
 
-	private int getNearestEmptyIndex(K key) {
-		final int entriesLength = entries.length;
-		int index = hash(key);
+            if (entries[index].key == key) {
+                final V previousValue = entries[index].value;
+                entries[index] = new Entry<>(key, value);
+                return previousValue;
+            }
 
-		while (entries[index] != null) {
-			index++;
-			if (index == entriesLength) {
-				index = 0;
-			}
-		}
+            index = index == entriesLength - 1 ? 0 : index + 1;
+        }
 
-		return index;
-	}
+        return null;
+    }
 
-	private static class Entry<K, V> {
+    public V get(Object key) {
+        final int entriesLength = entries.length;
+        final int startIndex = getIndex(key);
 
-		private final K key;
+        for (int index = startIndex; index != startIndex - 1; index++) {
+            if (entries[index] == null) {
+                return null;
+            }
 
-		private final V value;
+            if (entries[index].key == key) {
+                return entries[index].value;
+            }
 
-		private boolean isDeleted;
+            index = index == entriesLength - 1 ? 0 : index + 1;
+        }
 
-		public Entry(K key, V value) {
-			this.key = key;
-			this.value = value;
-			isDeleted = false;
-		}
-	}
+        return null;
+    }
+
+    public V remove(Object key) {
+        return null;
+    }
+
+    private int getIndex(Object key) {
+        return key.hashCode() % entries.length;
+    }
+
+    private void resize() {
+        final int newCapacity = Math.min(entries.length * 2, MAXIMUM_CAPACITY);
+        final Entry<K, V>[] newEntries = new Entry[newCapacity];
+
+        for (Entry<K, V> entry : entries) {
+            if (entry != null) {
+                newEntries[entry.key.hashCode() % newCapacity] = entry;
+            }
+        }
+
+        entries = newEntries;
+    }
+
+    private static class Entry<K, V> {
+
+        private final K key;
+
+        private final V value;
+
+        private boolean isDeleted;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+            isDeleted = false;
+        }
+    }
 }
--- task-08-hash-tables/src/test/java/ru/otus/HashTableTest.java
+++ task-08-hash-tables/src/test/java/ru/otus/HashTableTest.java
@@ -1,6 +1,7 @@
 package ru.otus;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,21 +12,48 @@ class HashTableTest {
 
 	@BeforeEach
 	void setUp() {
-		hashTable = new HashTable<>();
+		hashTable = new HashTable<>(3);
 	}
 
 	@Test
+	@DisplayName("Method size should return correct number of items")
 	void size() {
+		hashTable.put(1, "1");
+		hashTable.put(3, "3");
+		hashTable.put(5, "5");
+		hashTable.put(3, "6");
+
+		assertEquals(3, hashTable.size());
 	}
 
 	@Test
-	void put() {
-		hashTable.put(25, "Dog");
-		hashTable.put(27, "Cat");
-		hashTable.put(27, "Fox");
-		hashTable.put(25, "After Dog");
-		hashTable.put(25, "After Fox");
-		hashTable.put(25, "First of entries");
+	@DisplayName("Put with new item")
+	void put_new() {
+		final int key = 12;
+		final String value = "Value";
+
+		assertNull(hashTable.get(key));
+		assertEquals(0, hashTable.size());
+
+		hashTable.put(key, value);
+		assertEquals(value, hashTable.get(key));
+		assertEquals(1, hashTable.size());
+	}
+
+	@Test
+	@DisplayName("Put with existing item")
+	void put_existing() {
+		final int key = 12;
+		final String value = "Value";
+		final String newValue = "New value";
+
+		hashTable.put(key, value);
+		assertEquals(value, hashTable.get(key));
+		assertEquals(1, hashTable.size());
+
+		hashTable.put(key, newValue);
+		assertEquals(newValue, hashTable.get(key));
+		assertEquals(1, hashTable.size());
 	}
 
 	@Test
