1967783ac8281ca8c24b9425b5c98563a464dc93
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-02-15 22:30:17 +0200

    ALGO-10 Implemented depth-first search

---
+++ task-09-kosaraju-algorithm/src/main/java/ru/otus/DepthFirstSearch.java
@@ -0,0 +1,24 @@
+package ru.otus;
+
+import java.util.function.Consumer;
+
+public class DepthFirstSearch {
+
+	public static void search(int[][] graph, int startVertex, Consumer<Integer> visit) {
+		final boolean[] visited = new boolean[graph.length];
+		final Stack<Integer> stack = new Stack<>();
+		stack.push(startVertex);
+
+		while (!stack.empty()) {
+			final int vertex = stack.pop();
+
+			for (int adjacentVertex : graph[vertex]) {
+				if (!visited[adjacentVertex] && !stack.contains(adjacentVertex)) {
+					visit.accept(adjacentVertex);
+					visited[adjacentVertex] = true;
+					stack.push(adjacentVertex);
+				}
+			}
+		}
+	}
+}
