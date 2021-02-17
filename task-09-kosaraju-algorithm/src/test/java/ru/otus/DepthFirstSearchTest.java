cc8e80fadd05b9a2de6373584087d90151f5dea3
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-02-17 19:13:23 +0200

    ALGO-10 Implemented unit test for DFS

--- task-09-kosaraju-algorithm/src/test/java/ru/otus/DepthFirstSearchTest.java
+++ task-09-kosaraju-algorithm/src/test/java/ru/otus/DepthFirstSearchTest.java
@@ -2,14 +2,20 @@ package ru.otus;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepthFirstSearchTest {
 
-	private int[][] graph = {{1, 2, 3}, {0, 3, 4}, {0, 3}, {0, 1, 2, 4}, {1, 3}};
+    private final int[][] graph = {{1, 2, 3, 6}, {0, 7, 8}, {0, 3}, {0, 2, 5}, {5}, {3, 4}, {0}, {1, 8}, {1, 7}};
+    private final int[] expectedResult = {0, 6, 3, 5, 4, 2, 1, 8, 7};
 
-	@Test
-	void search() {
-		DepthFirstSearch.search(graph, 0, System.out::println);
-	}
+    @Test
+    void search() {
+        final List<Integer> result = new ArrayList<>(graph.length);
+        DepthFirstSearch.search(graph, 0, result::add);
+        assertArrayEquals(expectedResult, result.stream().mapToInt(Integer::intValue).toArray());
+    }
 }
