f8d59347cea038c216fa800db41ee07b2557978e
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-02-20 20:26:15 +0200

    ALGO-10 Implemented recursion depth-first search

--- util/src/main/java/ru/otus/graph/util/GraphUtils.java
+++ util/src/main/java/ru/otus/graph/util/GraphUtils.java
@@ -5,6 +5,23 @@ import java.util.function.Consumer;
 
 public class GraphUtils {
 
+    public static void depthFirstSearch(int[][] graph, int startVertex, Consumer<Integer> visitor) {
+        final boolean[] visited = new boolean[graph.length];
+        dfsRecursive(graph, startVertex, visitor, visited);
+    }
+
+    private static void dfsRecursive(int[][] graph, int vertex, Consumer<Integer> visitor, boolean[] used) {
+        used[vertex] = true;
+
+        for (int adjacentVertex : graph[vertex]) {
+            if (!used[adjacentVertex]) {
+                dfsRecursive(graph, adjacentVertex, visitor, used);
+            }
+        }
+
+        visitor.accept(vertex);
+    }
+
     public static int[][] transpose(int[][] graph) {
         final int[][] transposedGraph = new int[graph.length][];
 
--- util/src/test/java/ru/otus/graph/util/GraphUtilsTest.java
+++ util/src/test/java/ru/otus/graph/util/GraphUtilsTest.java
@@ -3,15 +3,31 @@ package ru.otus.graph.util;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GraphUtilsTest {
 
-    private final int[][] graph = {{1, 2}, {0, 2}, {0}};
+    private int[][] graph;
+
+    @BeforeEach
+    void setUp() {
+        graph = new int[][]{{1, 2, 3, 6}, {0, 7, 8}, {0, 3}, {0, 2, 5}, {5}, {3, 4}, {0}, {1, 8}, {1, 7}};
+    }
 
     @Test
     void transpose() {
-        final int[][] expectedResult = {{1, 2}, {0}, {0, 1}};
+        final int[][] expectedResult = {{1, 2, 3, 6}, {0, 7, 8}, {0, 3}, {0, 2, 5}, {5}, {3, 4}, {0}, {1, 8}, {1, 7}};
         assertArrayEquals(expectedResult, GraphUtils.transpose(graph));
+    }
+
+    @Test
+    void depthFirstSearchRecursive() {
+        final int[] expectedResult = {8, 7, 1, 4, 5, 3, 2, 6, 0};
+        final List<Integer> result = new ArrayList<>(graph.length);
+        GraphUtils.depthFirstSearch(graph, 0, result::add);
+        assertArrayEquals(expectedResult, result.stream().mapToInt(Integer::intValue).toArray());
     }
 }
