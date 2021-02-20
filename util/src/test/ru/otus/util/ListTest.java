41ecd26a03f70288cb449d65f88b20596641c8d1
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-02-20 22:38:56 +0200

    ALGO-15 Created data structure List

---
+++ util/src/test/ru/otus/util/ListTest.java
@@ -0,0 +1,45 @@
+package ru.otus.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListTest {
+
+	private List<Integer> list;
+
+	@BeforeEach
+	void setUp() {
+		list = new List<>();
+		list.add(100);
+		list.add(200);
+		list.add(300);
+		list.add(400);
+		list.add(500);
+	}
+
+	@Test
+	void add() {
+		list.add(600);
+		assertEquals(6, list.size());
+		assertEquals(600, list.get(list.size() - 1));
+	}
+
+	@Test
+	void get() {
+		assertEquals(300, list.get(2));
+	}
+
+	@Test
+	void remove() {
+		assertEquals(300, list.remove(2));
+		assertEquals(4, list.size());
+	}
+
+	@Test
+	void size() {
+		assertEquals(5, list.size());
+	}
+
+}
