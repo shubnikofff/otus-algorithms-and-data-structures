2e2b33fa4351a7a3fd2996665f8f707712272d58
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-03-07 20:32:39 +0200

    Task-11: Created class Edge

---
+++ task-11-minimum-spanning-tree/src/main/java/ru/otus/Edge.java
@@ -0,0 +1,33 @@
+package ru.otus;
+
+public class Edge {
+
+	private final int src;
+
+	private final int dst;
+
+	private final int weight;
+
+	public Edge(int src, int dst, int weight) {
+		this.src = src;
+		this.dst = dst;
+		this.weight = weight;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+
+		if (!(obj instanceof Edge)) {
+			return false;
+		}
+
+		final Edge edge = (Edge) obj;
+
+		return edge.weight == this.weight && (
+				edge.src == this.src && edge.dst == this.dst || edge.dst == this.src && edge.src == this.dst
+		);
+	}
+}
