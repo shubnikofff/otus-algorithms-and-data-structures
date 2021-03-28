a10f8ae0e16870b9c8d34119c8aa09d4ec374b14
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-03-28 22:59:29 +0300

    Task-13: Implemented substring slow search algorithm

---
+++ task-13-boyer-moore-algorithm/src/main/java/ru/otus/SubstringSlowSearch.java
@@ -0,0 +1,26 @@
+package ru.otus;
+
+public class SubstringSlowSearch {
+
+	public int find(String text, String pattern) {
+		int t = 0;
+		final int textLength = text.length();
+		final int patternLength = pattern.length();
+		final int last = patternLength - 1;
+
+		while (t < textLength - last) {
+			int p = 0;
+			while (p < last && text.charAt(t + p) == pattern.charAt(p)) {
+				p++;
+			}
+
+			if (p == patternLength) {
+				return t;
+			}
+
+			t++;
+		}
+
+		return -1;
+	}
+}
