31905113c6e7ac0202c5d44560f954aa56bef155
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-03-28 18:38:19 +0300

    Task-12: Implemented Dijkstras Algorithm

--- task-12-finding-shortest-path/src/main/java/ru/otus/DijkstrasAlgorithm.java
+++ task-12-finding-shortest-path/src/main/java/ru/otus/DijkstrasAlgorithm.java
@@ -1,25 +1,86 @@
 package ru.otus;
 
+import ru.otus.util.Array;
+import ru.otus.util.List;
 import ru.otus.util.graph.Edge;
 
 import java.util.Arrays;
 
 public class DijkstrasAlgorithm {
 
-	private final int[][] graph;
+    private final Integer[][] graph;
 
-	private final float[] distances;
+    private final int startVertex;
 
-	public DijkstrasAlgorithm(int[][] graph) {
-		this.graph = graph;
-		distances = new float[graph.length];
-	}
+    private final float[] distances;
 
-	private void init() {
-		Arrays.fill(distances, Float.POSITIVE_INFINITY);
-	}
+    private final int[] prev;
 
-	public Edge[] findShortestPath(int u, int v) {
-		distances[u] = 0f;
-	}
+    private final boolean[] used;
+
+    public DijkstrasAlgorithm(Integer[][] graph, int startVertex) {
+        this.graph = graph;
+        this.startVertex = startVertex;
+
+        distances = new float[graph.length];
+        Arrays.fill(distances, Float.POSITIVE_INFINITY);
+        distances[startVertex] = 0f;
+
+        prev = new int[graph.length];
+        used = new boolean[graph.length];
+
+        findShortestPaths();
+    }
+
+    public Edge[] getShortestPath(int destination) {
+        if (destination == startVertex) {
+            return new Edge[]{};
+        }
+
+        final List<Edge> path = new List<>(graph.length - 1);
+        int vertex = destination;
+
+        while (vertex != startVertex) {
+            int prev = this.prev[vertex];
+            path.add(new Edge(prev, vertex, graph[prev][vertex]));
+            vertex = prev;
+        }
+
+        final Edge[] result = path.toArray(new Edge[path.size()]);
+        Array.reverse(result);
+
+        return result;
+    }
+
+    private void findShortestPaths() {
+        Integer vertex = findMinNotUsedVertex();
+
+        while (vertex != null) {
+            for (int i = 0; i < graph[vertex].length; i++) {
+                final Integer neighborDistance = graph[vertex][i];
+                if (neighborDistance != null) {
+                    final float distance = distances[vertex] + neighborDistance;
+                    if (distance < distances[i]) {
+                        distances[i] = distance;
+                        prev[i] = vertex;
+                    }
+                }
+            }
+
+            used[vertex] = true;
+            vertex = findMinNotUsedVertex();
+        }
+    }
+
+    private Integer findMinNotUsedVertex() {
+        Integer min = null;
+
+        for (int i = 0; i < distances.length; i++) {
+            if (!used[i] && (min == null || distances[i] < distances[min])) {
+                min = i;
+            }
+        }
+
+        return min;
+    }
 }
