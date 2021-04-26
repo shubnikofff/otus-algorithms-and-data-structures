b0a1d7d37e65337f6a3da5122db12e3ef97a4ab6
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-04-26 22:42:26 +0300

    Task-16: Initial commit

---
+++ task-16-run-length-encoding/src/main/java/ru/otus/Archiver.java
@@ -0,0 +1,12 @@
+package ru.otus;
+
+public class Archiver {
+
+	public static void main(String[] args) {
+
+		if (args.length == 0) {
+			System.out.println("Please run as following:\njava -jar archiver.jar <input filename> <output filename>");
+		}
+
+	}
+}
