a289f35548100d3a52f1ed965a86c654fe4afb25
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-04-30 00:20:04 +0300

    Task-17: Added map print

--- task-17-dynamic-programming/src/main/java/ru/otus/square/BarnSquareCalculator.java
+++ task-17-dynamic-programming/src/main/java/ru/otus/square/BarnSquareCalculator.java
@@ -7,12 +7,14 @@ import java.time.Instant;
 
 public class BarnSquareCalculator {
 
-	private static final int MAP_WIDTH = 1000;
-	private static final int MAP_HEIGHT = 1000;
+	private static final int MAP_WIDTH = 10;
+	private static final int MAP_HEIGHT = 10;
 	private static final int OBSTACLE_FACTOR = 7;
 
 	public static void main(String[] args) {
 		final int[][] map = Matrix.makeRandomMatrix(MAP_WIDTH, MAP_HEIGHT, OBSTACLE_FACTOR);
+
+		Matrix.print(map);
 
 		run(new LinearSquareCalculator(map));
 		run(new HeightCountSquareCalculator(map));
