1f8365c4422011feccf93f732526a95e8028fd5d
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-04-30 00:15:38 +0300

    Task-17:Created runners for calculators

--- task-17-dynamic-programming/src/main/java/ru/otus/square/BarnSquareCalculator.java
+++ task-17-dynamic-programming/src/main/java/ru/otus/square/BarnSquareCalculator.java
@@ -7,20 +7,24 @@ import java.time.Instant;
 
 public class BarnSquareCalculator {
 
-	private static final int MAP_WIDTH = 10;
-	private static final int MAP_HEIGHT = 10;
+	private static final int MAP_WIDTH = 1000;
+	private static final int MAP_HEIGHT = 1000;
 	private static final int OBSTACLE_FACTOR = 7;
 
 	public static void main(String[] args) {
-
 		final int[][] map = Matrix.makeRandomMatrix(MAP_WIDTH, MAP_HEIGHT, OBSTACLE_FACTOR);
 
+		run(new LinearSquareCalculator(map));
+		run(new HeightCountSquareCalculator(map));
+		run(new WidthCountSquareCalculator(map));
+	}
+
+	private static void run(SquareCalculator calculator) {
+		System.out.println("\nRun with " + calculator.getClass().getSimpleName());
 		final Instant instant = Instant.now();
-//		final int square = new LinearSquareCalc(map).calculateSquare();
-		final int square = new HeightSquareCalculator(map).calculateSquare();
+		final int square = calculator.calculateSquare();
 		final long duration = Duration.between(instant, Instant.now()).toMillis();
 
-		Matrix.print(map);
 		System.out.println("Square: " + square);
 		System.out.println("Duration: " + duration + "ms");
 	}
